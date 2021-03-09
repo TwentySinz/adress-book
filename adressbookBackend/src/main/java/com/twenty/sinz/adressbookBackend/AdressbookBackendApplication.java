46d2cb2dd730e7b9cce409d0198ddee63fc8aefa
twentysinz <twenty.sinz@gmail.com>
2021-03-09 14:15:24 +0100

    Backend says hello

---
+++ adressbookBackend/src/main/java/com/twenty/sinz/adressbookBackend/AdressbookBackendApplication.java
@@ -0,0 +1,23 @@
+package com.twenty.sinz.adressbookBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class AdressbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdressbookBackendApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World");
+	}
+
+}
---
+++ adressbookBackend/src/test/java/com/twenty/sinz/adressbookBackend/AdressbookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twenty.sinz.adressbookBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdressbookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
