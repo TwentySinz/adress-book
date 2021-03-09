86ae565bb2a0b4b18f58f555363cea0c7c400401
twentysinz <twenty.sinz@gmail.com>
2021-03-09 16:54:35 +0100

    Add class Person

--- adressbookBackend/src/main/java/com/twenty/sinz/adressbookBackend/AdressbookBackendApplication.java
+++ adressbookBackend/src/main/java/com/twenty/sinz/adressbookBackend/AdressbookBackendApplication.java
@@ -1,10 +1,13 @@
 package com.twenty.sinz.adressbookBackend;
 
+import com.twenty.sinz.adressbookBackend.person.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,14 @@ public class AdressbookBackendApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Person> hello() {
+		return List.of(
+		        new Person(1L,
+                        "Horst",
+                        "Worst",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21)
+        );
 	}
 
 }
---
+++ adressbookBackend/src/main/java/com/twenty/sinz/adressbookBackend/person/Person.java
@@ -0,0 +1,87 @@
+package com.twenty.sinz.adressbookBackend.person;
+
+import java.time.LocalDate;
+
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(Long id,
+                  String firstName,
+                  String lastName,
+                  LocalDate dateOfBirth,
+                  Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+    }
+
+    public Person(String firstName,
+                  String lastName,
+                  LocalDate dateOfBirth,
+                  Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", age=" + age +
+                '}';
+    }
+}
