37869acc79acd479b4c27376364e312dc54827cf
twentysinz <twenty.sinz@gmail.com>
2021-03-15 19:05:14 +0100

    add header and footer

--- adressbookFrontend/src/app/app.module.ts
+++ adressbookFrontend/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { MaterialModule } from './material/material.module';
 import { AdressBookComponent } from './components/adress-book/adress-book.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AdressBookCardComponent } from './components/adress-book-card/adress-book-card.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdressBookComponent,
-    AdressBookCardComponent
+    AdressBookCardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
